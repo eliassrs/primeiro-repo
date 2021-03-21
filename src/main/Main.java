f0ff65151624158188e6d79e65890beb5f9cf334
Elias Moreira <moreira-s@hotmail.com>
2021-03-21 20:08:36 -0300

    primeira aula java

---
+++ src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Main app = new Main();
+		app.start();
+
+	}
+
+	private void start() {
+		
+		int idade = 1;
+		System.out.println("Maria Clara tem " + idade + " ano");
+
+	}
+
+}
